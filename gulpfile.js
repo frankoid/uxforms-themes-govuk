835052110692a99a57370da651c7d7dd61bfacf4
Martin Gladdish <martin@martingladdish.co.uk>
2015-04-18 14:26:05 +0100

    create an archive of everything in the target dir

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var del = require('del');
 var runSequence = require('run-sequence');
+var tar = require('gulp-tar');
+var gzip = require('gulp-gzip');
 
 gulp.task('sass', function () {
     gulp.src('./source/scss/*.scss')
@@ -18,6 +20,13 @@ gulp.task('clean', function(c) {
     del(['target'], c);
 });
 
+gulp.task('package', ['sass', 'static'], function(p) {
+	gulp.src('./target/**')
+		.pipe(tar('govuk.tar'))
+		.pipe(gzip())
+		.pipe(gulp.dest('./target/'))
+})
+
 gulp.task('default', ['clean'], function(d) {
-	runSequence(['sass', 'static'], d);
+	runSequence(['sass', 'static'], 'package', d);
 });
