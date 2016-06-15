d5c5d1b26b5496a5a4531d1642ac315db768f097
Anthony Munene <munene.anthony@gmail.com>
2016-06-15 15:43:47 +0100

    focus on error messages

---
+++ source/static/javascripts/uxforms.js
@@ -0,0 +1,17 @@
+// Only set focus for the error example pages
+if ($(".js-error-example").length) {
+
+  // If there is an error summary, set focus to the summary
+  if ($(".error-summary").length) {
+    $(".error-summary").focus();
+    $(".error-summary a").click(function(e) {
+      e.preventDefault();
+      var href = $(this).attr("href");
+      $(href).focus();
+    });
+  }
+  // Otherwise, set focus to the field with the error
+  else {
+    $(".error input:first").focus();
+  }
+}
