c6a30b9c8130dd978337a4262a5d65face7e7adb
Martin Gladdish <martin@martingladdish.co.uk>
2016-08-29 12:15:11 +0100

    Don't need to trigger visibility @ onChange for input:checkbox

--- source/static/javascripts/uxforms.js
+++ source/static/javascripts/uxforms.js
@@ -9,9 +9,6 @@ function addListeners(sectionUrl) {
     $("input:radio").change(function() {
         refreshVisibility(sectionUrl);
     });
-    $("input:checkbox").change(function() {
-        refreshVisibility(sectionUrl);
-    });
     $("select").change(function() {
         refreshVisibility(sectionUrl);
     });
