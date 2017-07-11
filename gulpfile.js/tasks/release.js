7841a90e44f68f4c38aa3cac18437071fd67674a
Sam Plews <the@codeite.com>
2017-07-11 11:47:56 +0100

    read credential from file

--- gulpfile.js/tasks/release.js
+++ gulpfile.js/tasks/release.js
@@ -3,7 +3,8 @@ var gulp = require('gulp'),
     pkg = require('../../package.json'),
     Bintray = require('bintray'),
     bump = require('gulp-bump'),
-    config = require('../config');
+    config = require('../config'),
+    fs = require('fs');
 
 gulp.task('release', function(d) {
     return runSequence(
@@ -19,11 +20,21 @@ gulp.task('bintray', function() {
     var localFile = config.TARGET_DIR + '/' + artifactName;
     var remotePath = '/files/x86/' + artifactName;
 
+    var homeDir = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+    var credentialsFile = fs.readFileSync(homeDir + '/.bintray/.credentials', 'utf8')
+        .split('\n')              // split into lines
+        .filter(x => x)           // filter out empty lines
+        .map(x => x.split(' = ')) // split lines on equals
+        .reduce((p, c) => {       // convert to object
+            p[c[0]] = c[1];
+            return p;
+        }, {});
+
     var repository = new Bintray({
-        username: process.env.BINTRAY_USERNAME,
-        apikey: process.env.BINTRAY_API_KEY,
-        organization: process.env.BINTRAY_ORGANIZATION,
-        repository: process.env.BINTRAY_REPOSITORY
+        username: credentialsFile.user,
+        apikey: credentialsFile.password,
+        organization: 'equalexperts',
+        repository: 'uxforms-releases'
     });
 
     return repository.uploadPackage(pkg.name, pkg.version, localFile, remotePath, true, false)
